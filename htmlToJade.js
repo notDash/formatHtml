00b183dff82e8d46da78620e87c9f96476e063a5
lishengyong <lishengyong@gomeplus.com>
2016-12-02 15:56:07 +0800

    update

--- htmlToJade.js
+++ htmlToJade.js
@@ -6,6 +6,7 @@
 		currLevel:0,
 		target:'',
 		type:'html',
+		indentType:'tab', // 缩进类型
 		/**
 		 * [init description]
 		 * @param  {[type]}  doms      [节点id集合]
@@ -15,23 +16,33 @@
 		 */
 		switchHtmlJade:function (clickDom, suffix) {
 			$(""+clickDom).click(function () {
-				var targetId = $(this).prev().attr("id");
+				var targetId = $(this).parent().children().first().attr("id");
 				var changeId = targetId?targetId.substr(0, targetId.indexOf(suffix)):'';
-				var value = this.value;
-				if(!value) {
+				var value = this.innerHTML;
+				$(this).parent().children(""+clickDom).each(function (index, dom) {
+					if(dom === event.target) {
+						$(this).addClass('actived');
+					} else {
+						$(dom).removeClass('actived');
+					}
+				});
+				/*if(!value) {
 					value = 'html';
-					this.value = 'html';
+					this.innerHTML = 'html';
 				}
 				if(value == 'html') {
-					this.value = value = 'jade';
+					this.innerHTML = value = 'jade';
 				} else if(value == 'jade') {
-					this.value = value = 'html';
-				} 
-				HtmlFormat.init([changeId], false, '_code', value);
+					this.innerHTML = value = 'html';
+				} */
+				HtmlFormat.handleSwitch([changeId], false, '_code', value);
 			});
 		},
-		init: function (doms, needRoot, suffix, type, clickDom) {
+		init: function (doms, needRoot, suffix, type, clickDom, indentType) {
 			this.switchHtmlJade(clickDom, suffix);
+			this.handleSwitch(doms, needRoot, suffix, type);
+		},
+		handleSwitch: function (doms, needRoot, suffix, type) {
 			for(var i = 0 ; i < doms.length; ++i) {
 				var currDom = doms[i];
 				var withSpace = 0;
@@ -42,19 +53,23 @@
 				this.handleFormatHtml(currDom, isRoot, withSpace, needRoot);
 				$('#' + this.target)[0].innerHTML = HtmlFormat.tempResultHtml + HtmlFormat.tempResultSuffixArr.join('<br>');
 				this.reset();
-			}
-			
+			}	
 		},
 		handleFormatHtml: function (jqDom, isRoot, withSpace, needRoot) {
 			var currNode,outerH,pre,space='';
 			if(isRoot) { // 如果是第一次传递html代码进来
 				jqDom = $(jqDom)[0]; // jqDom[0] 是根元素
 				withSpace = 0;
-			} else if(jqDom.nodeType === 1){
+			} else if(jqDom.nodeType === 1 || (jqDom.nodeType === 3 && jqDom.nodeValue && jqDom.nodeValue.trim().length > 0)) {
 				withSpace += 1;
 			} else {
 				return;
 			}
+			/*else if(jqDom.nodeType === 1){
+				withSpace += 1;
+			} else {
+				return;
+			}*/
 			if(withSpace < this.currLevel) {
 				// 如果dom childNode level改变的时候，去添加</xxx>
 				for(var i = 0; i < this.currLevel - withSpace; ++i) {
@@ -74,18 +89,11 @@
 						this.tempResultHtml += this.wrapWithFormatL(pre, true, withSpace);
 					}
 				}
-				space = '';
-				for(var i = 0; i < withSpace * 4; ++i) {
-					space += ' ';
-				}
+				space = this.generateIndent(withSpace);
 				if(isRoot && !needRoot) { // 暂存根节点，如果不需要，后续截取掉
 					withSpace = -1
-				} else {
-					if(this.type === 'jade') {
-						this.tempResultSuffixArr.unshift('');
-					} else {
-						this.tempResultSuffixArr.unshift(space + '&lt;/' + currNode.tagName.toLowerCase() + '&gt;');
-					}
+				} else if(!(this.type === 'jade')) {
+					this.tempResultSuffixArr.unshift(space + '&lt;/' + currNode.tagName.toLowerCase() + '&gt;');
 				}
 				for(var i =0 , len = jqDom.childNodes.length; i < len; i ++) {
 					this.handleFormatHtml(jqDom.childNodes[i], false, withSpace);	
@@ -97,8 +105,15 @@
 						this.tempResultHtml += this.nodeToJade(currNode, true, withSpace, false);
 					} else {
 						outerH = currNode.outerHTML;
-						pre = outerH.substr(0, outerH.split('').length - (currNode.tagName.length + 3));
-						this.tempResultHtml += this.wrapWithFormatL(pre, false, withSpace) + '&lt;/' + currNode.tagName.toLowerCase() + '&gt;<br>';
+						this.tempResultHtml += this.wrapWithFormatL(outerH, false, withSpace) +  '<br>';
+					}
+					return;
+				}
+				if(jqDom.nodeType === 3) {
+					if(this.type === 'jade') {
+						this.tempResultHtml += this.nodeToJade(jqDom, true, withSpace, false);
+					} else {
+						this.tempResultHtml +=  this.wrapWithFormatL(jqDom.nodeValue.trim(), false, withSpace) + '<br>';
 					}
 					return;
 				}
@@ -106,11 +121,8 @@
 			}
 		},
 		wrapWithFormatL: function (html, withBR, withSpace) {
-			// 生成withSpace * 4 个空格
-			var space = '';
-			for(var i = 0; i < withSpace * 4; ++i) {
-				space += ' ';
-			}
+			// 根据缩进类型生成相应长度的缩进，空格为4个，Tab为一个
+			var space = this.generateIndent(withSpace);
 			return withBR?space + html.replace(/[<]/g, '&lt;').replace(/[>]/g, '&gt;<br>'):space + html.replace(/[<]/g, '&lt;').replace(/[>]/g, '&gt;');
 		},
 		hasElementChileNodes: function (element) {
@@ -124,25 +136,30 @@
 			return false;
 		},
 		nodeToJade: function (node, withBR, withSpace, hasChild) {
-			// 生成withSpace * 4 个空格
-			var space = '',
-				content = '',
+			var space = '', // 生成的缩进
+				content = '', // 生成的jade内容
 				attrLen = 0,
 				nodeName = '',
 				tempAttr,
 				tempAttrClasss,
-				hasMoreAttr=false,
 				textContent = '',
-				i, j;
+				i, j,
+				attrObj = {otherAttr:[]}; // 用于存储非id, class之外的属性值
+			if(!node) {
+				return;
+			}	
 			if(!hasChild) {
 				textContent = node.textContent;
 			}
-			for(i = 0; i < withSpace * 4; ++i) {
-				space += ' ';
-			}
+			space = this.generateIndent(withSpace);
 			// 如果要转换jade模板，则去获取该节点的attributes
 			var attributes = node.attributes;
-				attrLen = attributes.length;
+			if(!attributes) {
+				// 如果是文本， 把文本里的回车加之后的空格替换为‘| ’
+				content = node.nodeValue.trim().replace(/\n\s*/g, '<br>'+ space +'| ');
+				return withBR?space + '| ' + content + '<br>':space +  '| ' + content;;
+			}
+			attrLen = attributes.length;
 			nodeName = node.nodeName.toLowerCase();
 			if((nodeName === 'div' && attrLen === 0) || nodeName !== 'div') {
 				// 如果是div的情况， 则不用写标签名。类直接加.className, id直接加#id
@@ -159,24 +176,36 @@
 						content += tempAttrClasss[i]?'.' + tempAttrClasss[i]:'';
 					}
 				} else {
-					if(!hasMoreAttr) {
-						hasMoreAttr = true;
-						content += '(' + tempAttr.name.toLowerCase() + '="' + tempAttr.nodeValue + '"';
-					} else {
-						content += ', ' + tempAttr.name.toLowerCase() + '="' + tempAttr.nodeValue + '"';
-					}
+					// 存储其他的属性名和值
+					attrObj.otherAttr.push(tempAttr.name.toLowerCase() + '="' + tempAttr.nodeValue + '"');
 				}
 			}
-			if(hasMoreAttr) {
-				content += ')';
+			// 拼接属性值
+			if(attrObj.otherAttr && attrObj.otherAttr.length > 0) {
+				content += '(' + attrObj.otherAttr.join(',') + ')';
 			}
+			// 拼接内容textContent
 			if(textContent) {
 				content += ' ' + textContent;
 			}
 
-			return withBR?space + content + '<br>':space + 'xxx';
-		}
-		,
+			return withBR?space + content + '<br>':space + content;
+		},
+		generateIndent: function (num) {
+			var space = '';
+			var result = '';
+			// 生成缩进类型
+			if(this.indentType === 'tab') {
+				space = '	'; 
+			} else {
+				space = ' ';
+				num = 4 * num;
+			}
+			for(i = 0; i < num; ++i) {
+				result += space;
+			}
+			return result;
+		},
 		reset: function () {
 			this.tempResultHtml = '';
 			this.tempResultSuffixArr = [];
